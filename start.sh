b19ff1ef18febda6cc4429779d20b958c47622de
Schevv <Schevv@gmx.de>
2025-03-21 10:06:15 +0100

    Updated wyoming-satellite

--- start.sh
+++ start.sh
@@ -12,10 +12,11 @@ screen -dmS wyoming-satellite script/run --debug \
   --mic-command 'arecord -D pulse -r 16000 -c 1 -f S16_LE -t raw' \
   --snd-command 'aplay -D pulse -r 22050 -c 1 -f S16_LE -t raw' \
   --wake-uri 'tcp://127.0.0.1:10400' \
-  --wake-word-name 'jarvis_v2' \
+  --wake-word-name 'hey_jarvis' \
   --awake-wav ../wav/beep_hi.wav \
   --done-wav ../wav/beep_lo.wav \
-  --timer-finished-wav ../wav/beep_error.wav
+  --timer-finished-wav ../wav/beep_error.wav \
+  --send-stats-seconds 60
 cd ..
 
 sleep 2s
@@ -23,8 +24,7 @@ sleep 2s
 
 cd wyoming-openwakeword
 screen -dmS wyoming-openwakeword script/run --debug \
-  --custom-model-dir '../wakewords' \
-  --preload-model 'jarvis_v2' \
+  --preload-model 'hey_jarvis' \
   --threshold 0.5 \
   --uri 'tcp://127.0.0.1:10400'
 cd ..
